f919f8f035064dda10eb3332d77c51a0494f9551
nasaaar8 <nasaaar8@gmail.com>
2016-11-22 09:50:00 +0100

    suma

---
+++ sumar/sumar.py
@@ -0,0 +1,9 @@
+'''
+Created on 22 nov. 2016
+
+@author: najib
+'''
+numero1=float(input("Escribe el primer numero:"))
+numero2=float(input("Escribe el segundo numero:"))
+suma=numero1+numero2
+print("La suma de los dos numeros es %f"%suma)
