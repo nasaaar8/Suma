0c8031585bd5521f4824d015244df8b038558e51
nasaaar8 <nasaaar8@gmail.com>
2016-11-22 09:55:29 +0100

    multiplicacion

--- sumar/sumar.py
+++ sumar/sumar.py
@@ -5,5 +5,5 @@ Created on 22 nov. 2016
 '''
 numero1=float(input("Escribe el primer numero:"))
 numero2=float(input("Escribe el segundo numero:"))
-suma=numero1+numero2
-print("La suma de los dos numeros es %f"%suma)
+multiplicacion=numero1*numero2
+print("La multiplicacion de los dos numeros es %f"%multiplicacion)
